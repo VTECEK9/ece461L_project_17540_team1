d03339463bad2f24369c30867de1c3a6161e6c97
John Doe <ashley.le2820@gmail.com>
2024-09-30 12:54:58 -0500

    attempt to add commits to login/signup page

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyLoginPage from './pages/MyLoginPage';
+
+const App = () => {
+    return(
+        <><div className="App" /><MyLoginPage /></>
+    );
+};
+
+export default App;
---
+++ client/src/pages/MyLoginPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './MyLoginPage.css'
+
+
+const MyLoginPage = () => {
+
+    return(
+        <div className="login-container">
+           <div className="login-section">
+            <h2>Welcome!</h2>
+            <form>
+
+                <div className = "login-credentials">
+                    <label htmlFor="username">Username</label>
+                    <input type = "username" id="username" required />
+                </div>
+
+                <div className = "login-credentials">
+                    <label htmlFor = "password">Password</label>
+                    <input type = "password" id = "password" required />
+                </div>
+                
+                <button type = "submit">Login</button>
+            </form>
+            <div className = "bottom">
+                <p>Don't have an account? <a href = "#" >Sign up</a></p>
+            </div>
+           </div>
+        </div>
+ 
+    );
+
+};
+
+export default MyLoginPage;
