24f854395c00dfb3aba19d1945b4388a3a156026
Hudson Gould <hudsongould1@gmail.com>
2024-11-13 18:16:16 -0600

    Changed the encryption to ceaser cipher

--- server/encryption.py
+++ server/encryption.py
@@ -1,12 +1,35 @@
-from cryptography.fernet import Fernet
 from hashlib import sha256
+
+#These are our very basic "keys"
+n = 13
+d = 1
 
 def encrypt(str):
     return sha256(str.encode()).hexdigest()
 
-def decrypt(str):
-    return None#fernet.decrypt(str.encode()).decode('utf-8')
+def encrypt(inputText):
+    reversed = inputText[::-1]
+    outputText = shift(reversed)
+    return outputText
 
-print(encrypt("gouldh"))
-print(encrypt("gouldh"))
-print(encrypt("bruh"))
+def decrypt(inputText):
+    outputText = shift(inputText)
+    reversed = outputText[::-1]
+    return reversed
+
+def shift(inputText):
+    toReturn = []
+    for ch in inputText:
+        idx = ord(ch)
+        if d == 1: #Shifting right
+            if idx+n > 126: 
+                idx = (idx+n)%127+34
+            else:
+                idx += n
+        elif d == -1: #Shifting left
+            if idx-n < 34:
+                idx = (idx-n)+93
+            else:
+                idx -= n
+        toReturn.append(chr(idx))
+    return "".join(toReturn)
